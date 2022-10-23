4cab553c3e08974d641bd31a76052d5f4c1ef6bd
Shihoru <48069034+Shihoru@users.noreply.github.com>
2022-10-23 20:02:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,27 +14,27 @@ bot.on("message",message => {
 	
     if (Clear.match(message)){
         return Clear.action(message)
-    }.catch(console.error)
+    }
 	
     if(message.content == "Salut"){
         message.reply("Hello :raised_hands:");
         console.log("Le bot dit bonjour");
-    }.catch(console.error)
+    }
      if(message.content == "MasterPine"){
         message.reply("@everyone");
         console.log("Le bot est pret pour la pine");
-    }.catch(console.error)
+    }
      if(message.content == "Paladins"){
         message.reply("@everyone go rush palouf !");
         console.log("Ready pour un paladin");
-    }.catch(console.error)
+    }
      if(message.content == "AmongUs"){
         message.reply("@everyone You looking sus ! https://media1.tenor.com/images/c5b612df9ef285c67d38c17ddd9635b9/tenor.gif?itemid=17674950");
         console.log("Ready pour un amongus");
-    }.catch(console.error)
+    }
 	if(message.content == "OW"){
         message.reply("@everyone Appel Overwatch");
         console.log("Ready pour un overwatchs");
-    }.catch(console.error)
+    }
 	
-    });
+    }).catch(console.error);
